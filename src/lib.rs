24088763f6728aff1120c36dd3a1b6a0fc7a7d80
Ben PHL <benphawke@gmail.com>
2023-07-23 19:48:18 +0200

    General cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,15 @@ use syn::token::Comma;
 use syn::{bracketed, Result};
 use syn::{parse_macro_input, Expr, ExprArray};
 
-/// Parses the tokens for an array of an array of expressions into manupilable vec
+/// Parses the tokens for an array of an array of expressions into a nested vec, which can then
+/// itself be transposed
 struct MatrixInput {
-    pub incoming_exprs: Vec<Vec<Expr>>, // The values in the matrix
+    pub incoming_exprs: Vec<Vec<Expr>>,
 }
 
 impl Parse for MatrixInput {
     fn parse(input: ParseStream) -> Result<Self> {
-        // pull out the bracket-content, i.e. the array expressions
+        // pull out the bracket-content, i.e. the array of the array of the expressions
         let content;
         let _ = bracketed!(content in input);
 
@@ -23,14 +24,22 @@ impl Parse for MatrixInput {
         let rows: Punctuated<ExprArray, Comma> =
             content.parse_terminated(ExprArray::parse, Comma)?;
 
-        // Error-at-compile-time if we try transposing something empty
+        // Error-at-compile-time if we try transposing something empty, or a 1x1 matrix
         let Some(fst) = rows.first() else {
             return Err(syn::Error::new_spanned(&rows, "Expected a non-empty matrix"));
         };
         if fst.elems.is_empty() {
             return Err(syn::Error::new_spanned(fst, "Expected a non-empty matrix"));
         }
+        if rows.len() == 1 && fst.elems.len() == 1 {
+            return Err(syn::Error::new_spanned(
+                fst,
+                "1x1 matrix transpose is a no-op",
+            ));
+        }
 
+        // We parsed the matrix, and sanity checked its dimensions. Now to return the expresions in
+        // vec-of-vec form to setup the data
         let mut matrix = vec![];
         for expr_array in rows {
             let mut row = vec![];
@@ -49,18 +58,12 @@ impl Parse for MatrixInput {
 
 #[proc_macro]
 pub fn transpose(input: TokenStream) -> TokenStream {
+    // post-condition: the dimensions have been sanity-checked to be suitable for transposition
     let MatrixInput { incoming_exprs } = parse_macro_input!(input as MatrixInput);
 
-    // Post-parsing check
-    if incoming_exprs.is_empty() || incoming_exprs[0].is_empty() {
-        let error = syn::Error::new(
-            proc_macro2::Span::call_site(),
-            "The input matrix should not be empty",
-        );
-        let compile_error = error.to_compile_error();
-        return compile_error.into();
-    }
-
+    // Do the actual transposition of expressions. We assume the (square) matrix is symmetrical
+    // untill contradicted. If never contradicted, we can error out: This macro is just over-head
+    // for no benefit.
     let mut still_sym = incoming_exprs.len() == incoming_exprs[0].len();
     let mut transposed = vec![vec![]; incoming_exprs[0].len()];
     for (i, incoming_row) in incoming_exprs.iter().enumerate() {
@@ -75,6 +78,8 @@ pub fn transpose(input: TokenStream) -> TokenStream {
     }
 
     if still_sym {
+        // Transposition being a no-op isn't depenedent on dimensions, but also symetry of
+        // values...
         return syn::Error::new(
             proc_macro2::Span::call_site(),
             "Uneccisary transpose: Matrix is symmetrical",
